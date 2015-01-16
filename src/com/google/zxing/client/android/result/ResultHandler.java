6b177e70c08adcb4bb8820653604a3dea0122e66
ben.upsilon <ben.upsilon@gmail.com>
2015-01-16 11:11:20 +0800

    删除book相关处理.

--- src/com/google/zxing/client/android/result/ResultHandler.java
+++ src/com/google/zxing/client/android/result/ResultHandler.java
@@ -29,10 +29,8 @@ import android.util.Log;
 import ben.upsilon.exp.aqr.R;
 import com.google.zxing.Result;
 import com.google.zxing.client.android.Contents;
-import com.google.zxing.client.android.Intents;
 import com.google.zxing.client.android.LocaleManager;
 import com.google.zxing.client.android.PreferencesActivity;
-import com.google.zxing.client.android.book.SearchBookContentsActivity;
 import com.google.zxing.client.result.ParsedResult;
 import com.google.zxing.client.result.ParsedResultType;
 import com.google.zxing.client.result.ResultParser;
@@ -402,25 +400,6 @@ public abstract class ResultHandler {
                 LocaleManager.getCountryTLD(activity) + "/maps?f=d&daddr=" + latitude + ',' + longitude)));
     }
 
-    // Uses the mobile-specific version of Product Search, which is formatted for small screens.
-    final void openProductSearch(String upc) {
-        Uri uri = Uri.parse("http://www.google." + LocaleManager.getProductSearchCountryTLD(activity) +
-                "/m/products?q=" + upc + "&source=zxing");
-        launchIntent(new Intent(Intent.ACTION_VIEW, uri));
-    }
-
-    final void openBookSearch(String isbn) {
-        Uri uri = Uri.parse("http://books.google." + LocaleManager.getBookSearchCountryTLD(activity) +
-                "/books?vid=isbn" + isbn);
-        launchIntent(new Intent(Intent.ACTION_VIEW, uri));
-    }
-
-    final void searchBookContents(String isbnOrUrl) {
-        Intent intent = new Intent(Intents.SearchBookContents.ACTION);
-        intent.setClassName(activity, SearchBookContentsActivity.class.getName());
-        putExtra(intent, Intents.SearchBookContents.ISBN, isbnOrUrl);
-        launchIntent(intent);
-    }
 
     final void openURL(String url) {
         // Strangely, some Android browsers don't seem to register to handle HTTP:// or HTTPS://.
